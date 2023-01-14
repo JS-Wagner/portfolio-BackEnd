0702f8765c1757ad70ab4206a52bb84452136769
J. Wagner <aguswagnercalvo@hotmail.com>
2023-01-14 13:39:10 -0300

    Rutas solucionadas

--- src/main/java/com/jwportfolio/jsw/Controller/CExperiencia.java
+++ src/main/java/com/jwportfolio/jsw/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explaboral")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontendprueba-291f0.web.app/home","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/jwportfolio/jsw/Controller/CProyecto.java
+++ src/main/java/com/jwportfolio/jsw/Controller/CProyecto.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://frontendprueba-291f0.web.app/home","http://localhost:4200"})
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
